9b56744342efb00cb18aa20d3f8663b301e7d187
Setthasiri Ngamsom <ngamsomset@Setthasiris-MacBook-Pro.local>
2021-05-09 18:51:00 +1000

    last commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,67 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from 'react'
+import { Button, Form, Container, Header } from 'semantic-ui-react'
+import axios from 'axios'
+import './App.css'
 
 function App() {
+  const [name, setName] = useState('')
+  const [age, setAge] = useState('')
+  const [salary, setSalary] = useState('')
+  const [hobby, setHobby] = useState('')
+
+  const handleSubmit = (e) => {
+    e.preventDefault()
+
+    const objt = { name, age, salary, hobby }
+
+    axios
+      .post(
+        'https://sheet.best/api/sheets/c8c8b864-2766-4560-8648-9dd0da4e2e7e',
+        objt
+      )
+      .then((response) => {
+        console.log(response)
+      })
+  }
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    <Container fluid className="container">
+      <Form className="form">
+        <Header as="h2">React Google Sheets</Header>
+        <Form.Field>
+          <label>Name</label>
+          <input
+            placeholder="Enter Your Name"
+            onChange={(e) => setName(e.target.value)}
+          />
+        </Form.Field>
+        <Form.Field>
+          <label>Age</label>
+          <input
+            placeholder="Enter Your Age"
+            onChange={(e) => setAge(e.target.value)}
+          />
+        </Form.Field>
+        <Form.Field>
+          <label>Salary</label>
+          <input
+            placeholder="Enter Your Salary"
+            onChange={(e) => setSalary(e.target.value)}
+          />
+        </Form.Field>
+        <Form.Field>
+          <label>Hobby</label>
+          <input
+            placeholder="Enter Your Hobby"
+            onChange={(e) => setHobby(e.target.value)}
+          />
+        </Form.Field>
+        <Button type="submit" color="blue" onClick={handleSubmit}>
+          Submit
+        </Button>
+      </Form>
+    </Container>
+  )
 }
 
-export default App;
+export default App
